96e9e2386c1b741cc9f2350b280b814ee013e3c4
pmtriit-it91 <pmtri.it91@gmail.com>
2024-02-20 11:33:10 +0700

    .firs-comit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { ColorModeContext, useMode } from './theme'
+import { Routes, Route } from 'react-router-dom'
+import Topbar from './scenes/global/Topbar'
+import Dashboard from './scenes/dashboard'
+import SidebarComponent from './scenes/global/SideBar'
+// import Sidebar from './scenes/global/SideBar'
+// import Team from './scenes/team'
+// import Invoices from './scenes/invoices'
+// import Contacts from './scenes/contacts'
+// import Bar from './scenes/bar'
+// import Form from './scenes/form'
+// import Line from './scenes/line'
+// import Pie from './scenes/pie'
+// import FAQ from './scenes/faq'
+// import Geography from './scenes/geography'
+// import Calendar from './scenes/calendar';
+
+
+function App() {
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SidebarComponent />
+          <main className='content'>
+            <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />}></Route>
+              {/* <Route path='/team' element={<Team />}></Route>
+              <Route path='/contacts' element={<Contacts />}></Route>
+              <Route path='/invoices' element={<Invoices />}></Route>
+              <Route path='/form' element={<Form />}></Route>
+              <Route path='/bar' element={<Bar />}></Route>
+              <Route path='/pie' element={<Pie />}></Route>
+              <Route path='/line' element={<Line />}></Route>
+              <Route path='/faq' element={<FAQ />}></Route>
+              <Route path='/geography' element={<Geography />}></Route>
+              <Route path='/calendar' element={<Calendar />}></Route> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+function Dashboard() {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+}
+
+export default Dashboard;
